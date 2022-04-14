cf5a5bc1731f5b413a45e1a8db2f814f97f7e260
Mustafa Siddiqui <mustafa.eins@gmail.com>
2022-04-14 10:09:26 -0400

    add read/write over SPI functions

--- inc/accel.h
+++ inc/accel.h
@@ -17,20 +17,56 @@
 #define _ACCEL_H_
 
 /* Register Maps */
-#define DEVID       (*((volatile unsigned char*) 0x00))  /* device ID => stores fixed value of 0xE5 */
-#define BW_RATE     (*((volatile unsigned char*) 0x2C))  /* data rate and power mode control */
-#define POWER_CTL   (*((volatile unsigned char*) 0x2D))  /* power-saving features control */
-#define DATA_FORMAT (*((volatile unsigned char*) 0x31))  /* data format control */
-#define DATA_X0     (*((volatile unsigned char*) 0x32))  /* x-axis data 0 */
-#define DATA_X1     (*((volatile unsigned char*) 0x33))  /* x-axis data 1 */
-#define DATA_Y0     (*((volatile unsigned char*) 0x34))  /* y-axis data 0 */
-#define DATA_Y1     (*((volatile unsigned char*) 0x35))  /* y-axis data 1 */
-#define DATA_Z0     (*((volatile unsigned char*) 0x36))  /* z-axis data 0 */
-#define DATA_Z1     (*((volatile unsigned char*) 0x37))  /* z-axis data 1 */
+#define _ADDR_DEVID       0x00  /* device ID => stores fixed value of 0xE5 */
+#define _ADDR_BW_RATE     0x2C  /* data rate and power mode control */
+#define _ADDR_POWER_CTL   0x2D  /* power-saving features control */
+#define _ADDR_DATA_FORMAT 0x31  /* data format control */
+#define _ADDR_DATA_X0     0x32  /* x-axis data 0 */
+#define _ADDR_DATA_X1     0x33  /* x-axis data 1 */
+#define _ADDR_DATA_Y0     0x34  /* y-axis data 0 */
+#define _ADDR_DATA_Y1     0x35  /* y-axis data 1 */
+#define _ADDR_DATA_Z0     0x36  /* z-axis data 0 */
+#define _ADDR_DATA_Z1     0x37  /* z-axis data 1 */
+
+#define DEVID       (*((volatile unsigned char*) _ADDR_DEVID))
+#define BW_RATE     (*((volatile unsigned char*) _ADDR_BW_RATE))
+#define POWER_CTL   (*((volatile unsigned char*) _ADDR_POWER_CTL))
+#define DATA_FORMAT (*((volatile unsigned char*) _ADDR_DATA_FORMAT))
+#define DATA_X0     (*((volatile unsigned char*) _ADDR_DATA_X0))
+#define DATA_X1     (*((volatile unsigned char*) _ADDR_DATA_X1))
+#define DATA_Y0     (*((volatile unsigned char*) _ADDR_DATA_Y0))
+#define DATA_Y1     (*((volatile unsigned char*) _ADDR_DATA_Y1))
+#define DATA_Z0     (*((volatile unsigned char*) _ADDR_DATA_Z0))
+#define DATA_Z1     (*((volatile unsigned char*) _ADDR_DATA_Z1))
 
 /* Utility Macros to set/clear indvidual bits in a register */
 #define SET(reg, bitNum)   (reg |= (1 << bitNum))
 #define CLEAR(reg, bitNum) (reg &= ~(1 << bitNum))
+
+/**
+ * @brief   Create first data byte that is transmitted over SPI for
+ *          accelerometer. byte => [R/W, MB, A5-A0]
+ * @param   RW: 1 or 0 => read/write bit 
+ * @param   MB: 1 or 0 => multiple byte transmission bit
+ * @param   addr: address of register to access 
+ * @return  content of the data byte to tranmsit
+ */
+unsigned char _ACCEL_createDataByte1(int RW, int MB, unsigned char addr);
+
+/**
+ * @brief   Write to a specified register on the accelerometer over SPI.
+ * @param   addr: address of the register to write to
+ * @param   data: data to write in the register
+ * @return  NULL 
+ */
+void _ACCEL_writeToRegister(unsigned char addr, unsigned char data);
+
+/**
+ * @brief   Read from a specified register on the accelerometer over SPI.
+ * @param   addr: address of the register to read from
+ * @return  content of the specified register 
+ */
+unsigned char _ACCEL_readFromRegister(unsigned char addr);
 
 /**
  * @brief   Get the device ID of the accelerometer sensor.
@@ -38,7 +74,7 @@
  * @param   NULL
  * @return  device ID of sensor
  */
-unsigned char _getDeviceID(void);
+unsigned char _ACCEL_getDeviceID(void);
 
 /**
  * @brief   Enable measurement mode on the accelerometer to
@@ -46,7 +82,7 @@ unsigned char _getDeviceID(void);
  * @param   NULL
  * @return  NULL
  */
-void _enableMeasureMode(void);
+void _ACCEL_enableMeasureMode(void);
 
 /**
  * @brief   Enable standby mode to disable measurement mode &
@@ -55,7 +91,7 @@ void _enableMeasureMode(void);
  * @param   NULL
  * @return  NULL
  */
-void _enableStandbyMode(void);
+void _ACCEL_enableStandbyMode(void);
 
 /**
  * @brief   Get current x, y, and z axis readings from the
@@ -64,11 +100,13 @@ void _enableStandbyMode(void);
  * @return  an integer array of length = 3
  *          => format: [x, y, z]
  */
-int* _getCurrentReading(void);
+int* _ACCEL_getCurrentReading(void);
 
 /**
- * @brief   Intialize accelerometer module to start taking
- *          measurement readings.
+ * @brief   Configure accelerometer's SPI module as slave
+ *          in line with PIC18's configurations as master.
+ *          Enables sensor measurements to be transmitted 
+ *          from the accelerometer.
  * @param   NULL
  * @return  1 if successfully initialized, 0 if not
  */
--- src/accel.c
+++ src/accel.c
@@ -13,13 +13,61 @@
 //-//
 #include <xc.h>             // OSCCON
 
+/* create first data byte for SDI line for accelerometer */
+unsigned char _ACCEL_createDataByte1(int RW, int MB, unsigned char addr) {
+    unsigned char dataByte1 = 0x0;
+    dataByte1 |= addr;
+
+    // set read/write bit
+    if (RW == 1)
+        SET(dataByte1, 7);
+    else
+        CLEAR(dataByte1, 7);
+
+    // set multiple byte transmission bit
+    if (MB == 1)
+        SET(dataByte1, 6);
+    else
+        CLEAR(dataByte1, 6);
+
+    return dataByte1;
+}
+
+/* write to register on accelerometer */
+void _ACCEL_writeToRegister(unsigned char addr, unsigned char data) {
+    // first byte consists of R/W, MB, and address bits (of the register to write to)
+    unsigned char dataByte_1 = _ACCEL_createDataByte1(0, 0, addr);
+
+    // latter byte is data to be written
+    unsigned char dataByte_2 = data;
+
+    _SPI_write(dataByte_1, ACCELEROMETER);
+    _SPI_write(dataByte_2, ACCELEROMETER);
+}
+
+/* read from register on accelerometer */
+unsigned char _ACCEL_readFromRegister(unsigned char addr) {
+    // first byte consists of R/W, MB, and address of register to read
+    unsigned char dataByte_1 = _ACCEL_createDataByte1(1, 0, addr);
+
+    // dummy data for transmission
+    unsigned char dataByte_2 = 0x00;
+
+    // write on MOSI line
+    _SPI_write(dataByte_1, ACCELEROMETER);
+    _SPI_write(dataByte_2, ACCELEROMETER);
+
+    // receive from MISO line
+    int length = 1;
+    unsigned char data[length];
+    _SPI_read(data, length);
+
+    return data[0];
+}
+
 /* get device ID */
 unsigned char _getDeviceID(void) {
-    // select register to read
-    SPI_Write(DEVID);
-
-    // read register and return value
-    return SPI_Read();    
+    return _ACCEL_readFromRegister(_ADDR_DEVID);
 }
 
 /* enable measurement mode */
@@ -47,6 +95,13 @@ int initAccel(void) {
     OSCCON = 0x72;
     _enableMeasureMode();
     // TODO: set more registers as required
+    // data_format reg:
+    //      select 4-wire mode -> bit D6 in data_format reg 
+    //      (full_res -- bit D3 -- might need to be set also)
+    //      range (bits D1 & D0) specify the range -- 2g, 4g, etc
+    // max SPI clock speed = 5 MHz => CONFLICT with current 8 MHz
+    // multiple byte reads possible -> set MB bit in first byte transfer
+    // select rate_bits in bw_rate to set output data rate
 
     // return 0 if cannot correctly read register
     if (_getDeviceID() != 0xE5) 
