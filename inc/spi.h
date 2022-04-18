731dc650ef5fe6c1e3a277df80b80c2303af2085
Mustafa Siddiqui <53434234+mustafa-siddiqui@users.noreply.github.com>
2022-04-17 23:02:40 -0400

    upload working spi communication header file

--- inc/spi.h
+++ inc/spi.h
@@ -1,8 +1,8 @@
 /**
  * @file    spi.h
- * @author  Ali Choudhry
+ * @author  Mustafa Siddiqui
  * @brief   Header file for SPI communication on PIC18 MCU.
- * @date    04/10/2022
+ * @date    04/14/2022
  * 
  * @copyright Copyright (c) 2022
  * 
@@ -11,45 +11,76 @@
 #ifndef _SPI_H_
 #define _SPI_H_
 
-#define CS LATA5
-#define LED LATD
+/* Serial Port Pins */
+#define _SPI_SDI TRISCbits.TRISC4  /* serial data in */
+#define _SPI_SDO TRISCbits.TRISC5  /* serial data out */
+#define _SPI_SCK TRISCbits.TRISC3  /* serial clock */
+
+#define _SPI_CS1 LATEbits.LATE2  /* chip select 1 */
+#define _SPI_CS2 LATDbits.LATD6  /* chip select 2 */
+
+/* macros to choose slave */
+#define ACCELEROMETER   1
+#define MAGNETOMETER    2
+
+/* in order to use built-in delay functions defined in 'xc.h' */
+#define _XTAL_FREQ 8000000
 
 /**
- * @brief 
- * @param
- * @return
+ * @brief   Enable SPI IO i.e. basically enable the serial port on the PIC18.
+ * @param   NULL
+ * @return  NULL
  */
-void SPI_Init_Master(void);
+void _SPI_enableIO(void);
 
 /**
- * @brief 
- * @param
- * @return
+ * @brief   Disable serial port function.
+ * @param   NULL
+ * @return  NULL
  */
-void SPI_Init_Slave(void);
+void _SPI_disableIO(void);
 
 /**
- * @brief 
- * @param
- * @return
+ * @brief   Initialize the SPI module on the PIC18. 
+ *          Master mode, clock polarity, data input sample phase, clock edge,
+ *          and clock rate are set.
+ * @param   NULL
+ * @return  NULL
  */
-void SPI_Write(unsigned char x);
+void initSPI(void);
 
 /**
- * @brief 
- * @param
- * @return
+ * @brief   Selects slave given that the PIC18 is in master mode by pulling
+ *          ~CS low. Should be set at start of transmission for the duration
+ *          of the transmission. Must be unselected at the end of the
+ *          transmission. => see _SPI_unselectSlave()
+ *          => DOES NOT configure slave device <=
+ * @param   slave: accelerometer or magnetometer
+ * @return  NULL
  */
-unsigned char SPI_Read(void);
+void _SPI_selectSlave(int slave);
 
-/* => replace this to use in-built _delay() function <=
-      can make a function like delay_1ms() if specific delay needed
-*/
 /**
- * @brief 
- * @param
- * @return
+ * @brief   Unselects slave. Should be used at the end of transmission.
+ * @param   slave: accelerometer or magnetometer
+ * @return  NULL
  */
-void MSdelay(unsigned int val);
+void _SPI_unselectSlave(int slave);
+
+/**
+ * @brief   Transmit 1 byte of data to already selected slave device.
+ *          => source: https://openlabpro.com/guide/spi-module-in-pic18f4550/
+ * @param   data: 1 byte
+ * @return  0 if successful write, -1 if collision occurred
+ */
+signed char _SPI_write(unsigned char data_out);
+
+/**
+ * @brief   Receive 1 byte of data from slave device
+ *          => source: https://openlabpro.com/guide/spi-module-in-pic18f4550/
+ * @param   NULL
+ * @return  byte read
+ */
+unsigned char _SPI_read(void);
 
 #endif /* _SPI_H_*/
