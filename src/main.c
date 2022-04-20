02908c4fb7c056b02f6a41a5ef96d026f2e2feee
AliChoudhry <92758026+AliChoudhry@users.noreply.github.com>
2022-04-20 16:49:40 -0400

    Add files via upload

--- src/main.c
+++ src/main.c
@@ -8,12 +8,16 @@
  * 
  */
 
-#include "../inc/init.h"
-#include "../inc/uart.h"
-#include "../inc/spi.h"
+#include "init.h"
+#include "spi.h"
+#include "mag.h"
+#include "uart.h"
 //-//
 #include <xc.h>
 #include <stdio.h>  // sprintf()
+#include <string.h> // memset()
+#include <stdlib.h>
+#include <math.h>
     
 #define _XTAL_FREQ 8000000  // 8 MHz
 
@@ -39,14 +43,41 @@ int main(void) {
     UART_send_str("SPI initialized...");
     __delay_ms(1000);
     
+    // initialize accelerometer for communication
+    int status1 = Mag_Initialize();
+    UART_send_str("MAG initialized...");
+    __delay_ms(1000);
+    
+    int status2 = initAccel();
+    UART_send_str("MAG initialized...");
+    __delay_ms(1000);
+    
     // turn off LEDs to indicate end of init process
     LATDbits.LATD2 = 0;
     LATDbits.LATD3 = 0;
-    __delay_ms(1000);
+    _delay(1000);
     
-    while (1) {
-        ;
+    if (status1) {
+        UART_send_str("Mag ID correct!");
+        LATDbits.LATD2 = 1;
+        __delay_ms(1000);
+        LATDbits.LATD2 = 0;
+        __delay_ms(1000);
+    }
+        if (status2) {
+        UART_send_str("Accel ID correct!");
+        LATDbits.LATD2 = 1;
+        __delay_ms(1000);
+        LATDbits.LATD2 = 0;
+        __delay_ms(1000);
     }
 
+    while (1) {
+        int peepee = GetAzimuth();
+        char dataStr[20];
+        sprintf(dataStr, "[%d]", peepee);
+        UART_send_str(dataStr);
+        __delay_ms(1000);
+    }
     return 0;
 }
