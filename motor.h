c4f9573cb7be12938e08ccc789c7062f45925515
mjumaev <95051951+mjumaev@users.noreply.github.com>
2022-04-22 20:38:47 -0400

    Add files via upload

---
+++ motor.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   motor.h
+ * Author: mahmudjumaev
+ *
+ * Created on April 21, 2022, 10:10 PM
+ */
+
+#ifndef MOTOR_H
+#define	MOTOR_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* MOTOR_H */
+
