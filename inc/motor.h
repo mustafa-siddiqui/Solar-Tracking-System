ab45f0fc01305dcac245d6ed6b727fb9e720e683
Mustafa Siddiqui <mustafa.eins@gmail.com>
2022-04-24 06:40:45 -0400

    add required fn templates

--- inc/motor.h
+++ inc/motor.h
@@ -15,7 +15,47 @@
 #define SET(reg, bitNum)   (reg |= (1 << bitNum))
 #define CLEAR(reg, bitNum) (reg &= ~(1 << bitNum))
 
-// TODO: Add function declarations with comment blocks
+enum dir {
+    CLOCKWISE,
+    COUNTER_CLOCKWISE
+};
+
+enum motorNum {
+    HORIZONTAL,
+    VERTICAL
+};
+
+// TODO: Add comment blocks
+
+void pwm_Init(float f);
+void set_duty_cycle(int fraction, float f, int dir);
+
+// TODO: Implement these functions - @mjumaev
+
+/**
+ * @brief   Move motor which controls horizontal motion in the
+ *          specified direction with a specified duty cycle.
+ */
+void moveHorizontalMotor(int dutyCycle, float freq, int dir);
+
+/**
+ * @brief   Move motor which controls vertical motion in the
+ *          specified direction with a specified duty cycle.
+ */
+void moveVerticalMotor(int dutyCycle, float freq, int dir);
+
+/**
+ * @brief   Move one of the motors to control horizontal or 
+ *          vertical motion in a specified direction with a
+ *          specified duty cycle.
+ *          => primary func to be called from main()
+ * @param   dutyCycle: value from 0 - 100
+ * @param   freq: frequency - required for pwm calculations
+ * @param   dir: direction => CLOCKWISE or COUNTER_CLOCKWISE 
+ * @param   motorNum: which motor to move => HORIZONTAL or VERTICAL
+ * @return  NONE
+ */
+void moveMotor(int dutyCycle, float freq, int dir, int motorNum);
 
 #endif	/* MOTOR_H */
 
--- src/motor.c
+++ src/motor.c
@@ -11,6 +11,7 @@
 #include "../inc/motor.h"
 #include "../inc/uart.h"    // added for debugging
 //-//
+// TODO: remove unused libraries
 #include <xc.h>
 #include <stdio.h>  // sprintf()
 #include <math.h>   // sqrt()
@@ -18,45 +19,48 @@
 #include <string.h>
 
 void pwm_Init(float f) {
-    TRISCbits.RC2=0; //Setting RC2/CCP1 pin as an output
-    PORTCbits.RC2=0;
+    TRISCbits.RC2 = 0; //Setting RC2/CCP1 pin as an output
+    PORTCbits.RC2 = 0;
     
-    //float Tosc=1/(8*10^(6));
-    int Prescale=16;
-    //float f = 8e3;
+    int Prescale = 16;
     float PWM_Period = 1/f;
+
     //PR2=(PWM_Period/(4*Tosc*Prescale))-1;
- 
     //int num = (10^(-3)*8*10^6)/(4*16)-1;
     
     int num = (int)(PWM_Period * _XTAL_FREQ)/(4 * Prescale) - 1;
-    PR2=(unsigned char)(num);//10^(-3)/(4*(1/(8*10^6))*16)-1;
+    PR2 = (unsigned char)(num); // 10^(-3) / (4 * (1/(8*10^6)) * 16) - 1;
+
+    // debug statement
     char prStr[15];
     sprintf(prStr, "PR2: %x", PR2);
     UART_send_str(prStr);
     __delay_ms(100);
+
     //PR2=0b01111100; //PR2=(PWM2_Period/(4*Tosc*Prescale))-1=10^(-3)/(4*(1/8*10^6)*16)-1=124
-    T2CON=0b00000111; //Prescalar set to 16
-    
+    T2CON = 0b00000111; // Prescalar set to 16
 }
 
 /*
  accepts values between 0-100
  */
 void set_duty_cycle(int fraction, float f, int dir) {
+    // debug statement
     UART_send_str("calling set duty cycle function");
-    TRISCbits.RC1=0; //Setting RC2/CCP1 pin as an output
-    PORTCbits.RC1=dir;
+    
+    TRISCbits.RC1 = 0;  // Setting RC2/CCP1 pin as an output
+    PORTCbits.RC1 = dir;
+
     //char foscVal[10];
     //sprintf(foscVal, "fosc Val: %lf", fosc);
     //UART_send_str(foscVal);
     
-    int Prescale=16;
-    //float f=8e3;
-    float PWM_Period=1/f;
+    int Prescale = 16;
+    float PWM_Period = 1/f;
     
-    //int desired_CCPR = PWM_Period*(fraction/100)/(Tosc*Prescale)= PWM_Period*(fraction)*fosc/(100*Prescale);
+    // desired_CCPR = PWM_Period*(fraction/100)/(Tosc*Prescale) = PWM_Period*(fraction)*fosc/(100*Prescale)
     int desired_CCPR = (int)((PWM_Period * fraction * _XTAL_FREQ)/(100 * Prescale));
+
     //Duty_cycle/(Tosc*Prescalar)=10^(-3)*0.5/(1/(8/4*10^6)*16)=250 (0011111010) = 500*0.5 //check this formula
     //CCPR1L=0b00111110;
     
@@ -65,20 +69,44 @@ void set_duty_cycle(int fraction, float f, int dir) {
     sprintf(prStr, "CCPR: %x", desired_CCPR);
     UART_send_str(prStr);
     
+    // TODO: add description
     CCPR1L = (desired_CCPR & 0xFC )>> 2;
     CCP1CON = 0b00101100;
     
+    // TODO: add description
     if( desired_CCPR & 0b1){
         SET(CCP1CON, 4);
     } else {
         CLEAR(CCP1CON, 4);
     }
     
-    
-    
+    // TODO: add description
     if (desired_CCPR & 0b10) {
         SET(CCP1CON, 5); 
     } else {
         CLEAR(CCP1CON, 5); 
+    }
+}
+
+void moveHorizontalMotor(int dutyCycle, float freq, int dir) {
+    // TODO: implement this function
+}
+
+void moveVerticalMotor(int dutyCycle, float freq, int dir) {
+    // TODO: implement this function
+}
+
+/* move one of the motors in specified direction */
+void moveMotor(int dutyCycle, int dir, int motorNum) {
+    switch (motorNum) {
+        case HORIZONTAL:
+            moveHorizontalMotor(dutyCycle, freq, dir);
+            break;
+        case VERTICAL:
+            moveVerticalMotor(dutyCycle, freq, dir);
+            break;
+        default:
+            // do nothing if incorrect motor
+            break;
     }
 }
