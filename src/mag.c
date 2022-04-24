ca9fe08b019032aa15ac1005932b0080b2a5bc2a
Mustafa Siddiqui <53434234+mustafa-siddiqui@users.noreply.github.com>
2022-04-24 17:04:31 -0400

    upload most recent code

--- src/mag.c
+++ src/mag.c
@@ -1,15 +1,15 @@
 /**
  * @file    mag.c
  * @author  Mustafa Siddiqui
- * @brief   Function definitions for accelerometer module.
+ * @brief   Function definitions for magnetometer module.
  * @date    04/12/2022
  * 
  * @copyright Copyright (c) 2022
  * 
  */
-#include "../inc/spi.h"
-#include "../inc/mag.h"
-#include "../inc/uart.h"
+#include "spi.h"
+#include "mag.h"
+#include "uart.h"
 //-//
 #include <xc.h>
 #include <stdio.h>  // sprintf()
@@ -46,13 +46,13 @@ signed char MAG_Write(unsigned char address, unsigned char data_transmit) {
     
     /* CHANGE it to MAGNETOMETER */
     // start transmission by pulling ~CS low
-    _SPI_selectSlave(ACCELEROMETER);
+    _SPI_selectSlave(MAGNETOMETER);
     
     signed char status1 = _SPI_write(RW_Address);
     signed char status2 = _SPI_write(data_transmit);
 
     // end transmission by setting ~CS high
-    _SPI_unselectSlave(ACCELEROMETER);
+    _SPI_unselectSlave(MAGNETOMETER);
     
     // write not successful; collision occurred somewhere
     if ((status1 != 0) || (status2 != 0)) {
@@ -61,7 +61,7 @@ signed char MAG_Write(unsigned char address, unsigned char data_transmit) {
     return 0;
 }
 
-/* read from register on accelerometer */
+/* read from register on magnetometer */
 //Read/Write bit of 1 is to read
 unsigned char MAG_Read(unsigned char address) {
     // to store received data
@@ -72,14 +72,14 @@ unsigned char MAG_Read(unsigned char address) {
     
     /* Change it to MAGNETOMETER */
     // start transmission by pulling ~CS low
-    _SPI_selectSlave(ACCELEROMETER);
+    _SPI_selectSlave(MAGNETOMETER);
     
     // write on MOSI line & receive on MISO line
     _SPI_write(RW_Address);
     recieved_data = _SPI_read();
     
     // end transmission by setting ~CS high
-    _SPI_unselectSlave(ACCELEROMETER); 
+    _SPI_unselectSlave(MAGNETOMETER); 
     
     return recieved_data;
 }
@@ -92,8 +92,8 @@ unsigned char Get_MAG_ID(void) {
 void MAG_Data(int* sensorData) {
     // TODO
     //OUT Registers provide us with Magnetometer raw data
-    unsigned char X1 = MAG_Read(OUTX_L_REG);    //L--> Least Significant Bit
-    unsigned char X2 = MAG_Read(OUTX_H_REG);    //H--> Most Significant Bit
+    unsigned char X1 = MAG_Read(OUTX_L_REG);    //L--> Low Bits
+    unsigned char X2 = MAG_Read(OUTX_H_REG);    //H--> High Bits
     unsigned char Y1 = MAG_Read(OUTY_L_REG);
     unsigned char Y2 = MAG_Read(OUTY_H_REG);
     unsigned char Z1 = MAG_Read(OUTZ_L_REG);
@@ -101,19 +101,38 @@ void MAG_Data(int* sensorData) {
     
     // combine high and low values into one number
     // bits represent signed 2's complement format
-    int x = (X2 << 8) | X1;
-    int y = (Y2 << 8) | Y1;
-    int z = (Z2 << 8) | Z1;
-    
+    float x = (X2 << 8) | X1;
+    float y = (Y2 << 8) | Y1;
+    float z = (Z2 << 8) | Z1;
     // populate var
     memset(sensorData, 0, 4);
     sensorData[0] = x;
     sensorData[1] = y;
     sensorData[2] = z;
     
+    //__delay_ms(1000);
+    float Angle = 0;
+    float divider = (y/x); 
+    Angle = atan(divider);
+    Angle = Angle*(180/PI);// + DECLINATION;
+    
+    if (Angle > 360){
+        Angle = Angle - 360;
+    }
+    else if (Angle < 0){
+        Angle = Angle + 360;
+    }
+    //__delay_ms(1000);
+    sensorData[3] = Angle;
+}
+/*
+int MAG_Angle(void) {
+    int sensorData[4] = {0};
+    MAG_Data(sensorData);
+    
     //Convert data into angle form
     double Angle;
-    Angle = atan2( (double)y, (double)x );
+    Angle = atan2( (double)sensorData[0], (double)sensorData[1] );
     Angle = Angle*(180/PI);
     
     if (Angle > 360){
@@ -122,28 +141,28 @@ void MAG_Data(int* sensorData) {
     else if (Angle < 0){
         Angle = Angle + 360;
     }
-    sensorData[3] = (int)Angle;
+    return Angle;
     /*
-    If D is greater than 337.25 degrees or less than 22.5 degrees ? North
-    If D is between 292.5 degrees and 337.25 degrees ? North-West
-    If D is between 247.5 degrees and 292.5 degrees ? West
-    If D is between 202.5 degrees and 247.5 degrees ? South-West
-    If D is between 157.5 degrees and 202.5 degrees ? South
-    If D is between 112.5 degrees and 157.5 degrees ? South-East
-    If D is between 67.5 degrees and 112.5 degrees ? East
-    If D is between 0 degrees and 67.5 degrees ? North-East
+    If D is greater than 337.25 degrees or less than 22.5 degrees --> North
+    If D is between 292.5 degrees and 337.25 degrees --> North-West
+    If D is between 247.5 degrees and 292.5 degrees --> West
+    If D is between 202.5 degrees and 247.5 degrees --> South-West
+    If D is between 157.5 degrees and 202.5 degrees --> South
+    If D is between 112.5 degrees and 157.5 degrees --> South-East
+    If D is between 67.5 degrees and 112.5 degrees --> East
+    If D is between 0 degrees and 67.5 degrees --> North-East
     */
-}
+//}
 
-/* initialize accelerometer module */
+/* initialize magnetometer module */
 int Mag_Initialize(void) {
     
-    MAG_Write(CFG_REG_A, 0x80);
+    MAG_Write(CFG_REG_A, 0x00);
     //Set magnetometer to enable temperature compensation, normal mode,
     //Don't reset registers, high-resolution mode, 100Hz output, and
-    //single measurement mode
+    //continuous measurement mode
     
-    MAG_Write(CFG_REG_C, 0x34);
+    MAG_Write(CFG_REG_C, 0x36);
     //Default interrupts, inhibit I2C (SPI only), avoid reading incorrect data,
     //Don't invert high and low bits of data, 4-Wire SPI mode, enable self-test,
     //Default data-ready
--- src/main.c
+++ src/main.c
@@ -8,16 +8,20 @@
  * 
  */
 
-#include "../inc/init.h"
-#include "../inc/spi.h"
-#include "../inc/mag.h"
-#include "../inc/uart.h"
+#include "init.h"
+#include "spi.h"
+#include "mag.h"
+#include "uart.h"
+#include "accel.h"
+#include "azimuth.h"
+#include "lcd.h"
 //-//
 #include <xc.h>
 #include <stdio.h>  // sprintf()
-#include <string.h> // memset()
-#include <stdlib.h>
+#include <string.h>
+#include <stdlib.h> // memset()
 #include <math.h>
+#include <string.h>
     
 #define _XTAL_FREQ 8000000  // 8 MHz
 
@@ -31,8 +35,10 @@ int main(void) {
     // turn on LEDs to indicate start of init process
     LATDbits.LATD2 = 1;
     LATDbits.LATD3 = 1;
-    __delay_ms(1000);
-
+    __delay_ms(1500);
+    LATDbits.LATD2 = 0;
+    LATDbits.LATD3 = 0;
+/*
     // initialize UART module
     UART_RX_Init();
     UART_send_str("UART initialized...");
@@ -44,8 +50,12 @@ int main(void) {
     __delay_ms(1000);
     
     // initialize accelerometer for communication
-    int status = Mag_Initialize();
+    int status1 = Mag_Initialize();
     UART_send_str("MAG initialized...");
+    __delay_ms(1000);
+    
+    int status2 = initAccel();
+    UART_send_str("Accel initialized...");
     __delay_ms(1000);
     
     // turn off LEDs to indicate end of init process
@@ -53,32 +63,52 @@ int main(void) {
     LATDbits.LATD3 = 0;
     _delay(1000);
     
-    if (status) {
-        UART_send_str("Device ID correct!");
+    if (status1) {
+        UART_send_str("Mag ID correct!");
+        LATDbits.LATD2 = 1;
+        __delay_ms(1000);
+        LATDbits.LATD2 = 0;
+        __delay_ms(1000);
+    }
+    if (status2) {
+        UART_send_str("Accel ID correct!");
         LATDbits.LATD2 = 1;
         __delay_ms(1000);
         LATDbits.LATD2 = 0;
         __delay_ms(1000);
     }
     
-    char str[20];
-    sprintf(str, "CFG_REG_A: %x", MAG_Read(CFG_REG_A));
-    UART_send_str(str);
-    __delay_ms(1000);
+    */
+        char tt[] = "hello"; 
+        lcd_display_driver_initialize();                   /* Initialize 16x2 LCD */
+        __delay_ms(3000);
+        LATDbits.LATD2 = 1;
+        display_driver_use_first_line();
+        lcd_display_driver_write( tt, 5 );
+        
+        while(1);
     
-    memset(str, 0, sizeof(str));
-    sprintf(str, "CFG_REG_C: %x", MAG_Read(CFG_REG_C));
-    UART_send_str(str);
-    __delay_ms(1000);
     
-    int sensorData[4] = {0};
+    /*
+    int sensorDataMag[4] = {0};
+    int sensorDataAccel[3] = {0};
     while (1) {
-        MAG_Data(sensorData);
+        MAG_Data(sensorDataMag);
         char dataStr[20];
-        sprintf(dataStr, "[%d, %d, %d, %d]", sensorData[0], sensorData[1], sensorData[2], sensorData[3]);
+        sprintf(dataStr, "Mag Data: [%d, %d, %d, %d]", sensorDataMag[0], sensorDataMag[1], sensorDataMag[2], sensorDataMag[3]);
         UART_send_str(dataStr);
+        
+        _ACCEL_getCurrentReading(sensorDataAccel);
+        char dataStr2[20];
+        sprintf(dataStr2, "Accel Data: [%d, %d, %d]", sensorDataAccel[0], sensorDataAccel[1], sensorDataAccel[2]);
+        UART_send_str(dataStr2);
+        
+        //int peepee = GetAzimuth();
+        //char dataStr[20];
+        //sprintf(dataStr, "[%d]", peepee);
+        //UART_send_str(dataStr);
         __delay_ms(1000);
-    }
-
+    }//
+     */ 
     return 0;
 }
