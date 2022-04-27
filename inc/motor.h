16bdb094a213d10068c597cf57f0716b2ca9ff6c
mjumaev <95051951+mjumaev@users.noreply.github.com>
2022-04-27 17:10:10 -0400

    Add files via upload

--- inc/motor.h
+++ inc/motor.h
@@ -10,8 +10,6 @@
 
 #define _XTAL_FREQ 8000000
 
-//#define DUTY_CYCLE_CONST 5
-
 #define SET(reg, bitNum)   (reg |= (1 << bitNum))
 #define CLEAR(reg, bitNum) (reg &= ~(1 << bitNum))
 
@@ -25,25 +23,33 @@ enum motorNum {
     VERTICAL
 };
 
-// TODO: Add comment blocks
 
-void pwm_Init(float f);
-void motor1_duty_cycle(int fraction, int dir);
-void motor2_duty_cycle(int fraction, int dir);
 
-// TODO: Implement these functions - @mjumaev
+void pwm_Init(void);
 
 /**
  * @brief   Move motor which controls horizontal motion in the
  *          specified direction with a specified duty cycle.
  */
-void moveHorizontalMotor(int dutyCycle, float freq, int dir);
+void moveHorizontalMotor(int dutyCycle, int dir);
 
 /**
  * @brief   Move motor which controls vertical motion in the
  *          specified direction with a specified duty cycle.
  */
-void moveVerticalMotor(int dutyCycle, float freq, int dir);
+void moveVerticalMotor(int dutyCycle, int dir);
+
+/**
+ * @brief   Stop the Vertical Motor by setting the Duty Cycle
+ *          to 0%.
+ */
+void stopVerticalMotor(void);
+
+/**
+ * @brief   Stop the Horizontal Motor by setting the Duty Cycle
+ *          to 0%.
+ */
+void stopHorizontalMotor(void);
 
 /**
  * @brief   Move one of the motors to control horizontal or 
@@ -56,7 +62,7 @@ void moveVerticalMotor(int dutyCycle, float freq, int dir);
  * @param   motorNum: which motor to move => HORIZONTAL or VERTICAL
  * @return  NONE
  */
-void moveMotor(int dutyCycle, float freq, int dir, int motorNum);
+void moveMotor(int dutyCycle, int dir, int motorNum);
 
 #endif	/* MOTOR_H */
 
