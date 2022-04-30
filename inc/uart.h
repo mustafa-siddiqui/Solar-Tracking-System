34f7a8f448402a0433ea6d07c89f275fd062aac9
Mustafa Siddiqui <53434234+mustafa-siddiqui@users.noreply.github.com>
2022-04-29 23:46:49 -0400

    update comment block

--- inc/uart.h
+++ inc/uart.h
@@ -1,29 +1,24 @@
-/* 
- * File:   uart.h
- * Author: SolarPv-2022
- * Comments: functions for uart transmit and receive
+/**
+ * @file   uart.h
+ * @brief  Functions for UART transmission & receival.
+ * @author Carter Bordeleau
+ * @date   04/23/2022
  */
  
 #ifndef UART_H
 #define	UART_H
-
 
 #include <xc.h>
 #include <pic18.h>
 #include <string.h>
 #include <stdlib.h>
 
-//void __interrupt() UARTRX_ISR();
-
+// fn declarations
 char UART_Read_char(void);
 void UART_RX_Init(void);
 void UART_send_char(char );
 void UART_send_str(const char *);
 
-
 char UART_buffer[100];
-
-
-
 
 #endif	/* UART_H */
